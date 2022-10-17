dabd4b30fba2fa085375b187bc0aacb2b5066adc
bilaleluneis <bilaleluneis@gmail.com>
2022-10-16 20:42:02 -0700

    - example using move and reference (borrow) semantics

---
+++ src/borrow_checker_basics.rs
@@ -0,0 +1,34 @@
+/*
+@author: Bilal El Uneis
+@since: Oct 2022
+@email: bilaleluneis@gmail.com
+*/
+
+#![allow(dead_code)]
+
+// this function will take value and move it from caller (own it)
+fn move_value_from_caller(value: String) -> String { return value; }
+
+fn borrow_value_from_caller(_: &String) {}
+
+#[cfg(test)]
+mod borrow_checker_basics_tests {
+
+    use crate::borrow_checker_basics::{borrow_value_from_caller, move_value_from_caller};
+
+    #[test]
+    fn test_basic_move_semantics() {
+        let s = String::from("testString");
+        assert_eq!(move_value_from_caller(s), "testString");
+        /* uncomment bellow to see move error */
+        //assert_eq!(s, "testString");
+    }
+
+    #[test]
+    fn test_basic_borrow_sematics() {
+        let s = String::from("testString");
+        borrow_value_from_caller(&s);
+        assert_eq!(s, "testString");
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+mod borrow_checker_basics;
 mod r#struct;
+mod type_extension;
 
 // this must be present for binary project
 fn main() {}
